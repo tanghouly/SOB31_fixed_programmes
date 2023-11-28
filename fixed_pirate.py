02e01d5b5ad56de4ebfdb25c51021cd0afb83d3a
tanghouly <kdeebest@gmail.com>
2023-11-28 15:08:41 +0400

    Fixed pirate code with comments

---
+++ fixed_pirate.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?")#added double quotes for print
+if greeting not in ("Arrr!"):#added not for opposite of the operation 
+	print("Go away, pirate.")
+else:#elif changed to else
+        print("Greetings, lover  of pirates!")#logical error if password is correct it should be opposite
